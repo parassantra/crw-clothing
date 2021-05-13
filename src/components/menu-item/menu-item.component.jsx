9297e295b7c2141e59e187b4454c6f4a7859448b
Paras Santra <parassantra@Parass-iMac.local>
2021-05-12 21:50:50 -0400

    setup project and initial layout

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './menu-item.styles.scss'
+
+export const MenuItem = ({title, image, size}) => (
+    <div className={`${size} menu-item`}>
+        <div className='background-image'
+            style={{
+                backgroundImage: `url(${image})`,
+            }}
+        >
+
+        </div>
+        <div className='content'>
+          <div className='title'>{title.toUpperCase()}</div>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+    </div>    
+)
+
+
+export default MenuItem;
