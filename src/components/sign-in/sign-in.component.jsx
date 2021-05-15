38446f90336cb70840e7e574c4eedc0e709068df
Paras Santra <parassantra@Parass-iMac.local>
2021-05-15 00:46:58 -0400

    add handleSubmit for signin

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import './sign-in.styles.scss'
@@ -13,12 +13,18 @@ class Signin extends Component {
         }
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.setState({
-            email: '',
-            password: ''
-        })
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({
+                email:'',
+                password:''
+            })
+        }catch(err){
+            console.error(err);
+        } 
     }
 
     handleChange =  e => {
