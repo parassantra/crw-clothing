a4649046d183bef0443e9e30c90db2d2c40ca7a6
Paras Santra <parassantra@Parass-iMac.local>
2021-05-18 01:15:37 -0400

    add cart utils

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import CustomButton from '../custom-button/custom-button.component';
 import './collection-item.styles.scss'
-const CollectionItem = ({name, imageUrl, price}) => {
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions';
+
+const CollectionItem = ({ item, addItem}) => {
+    const {name, imageUrl, price} = item
     return (
         <div className='collection-item'>
             <div
@@ -14,8 +19,13 @@ const CollectionItem = ({name, imageUrl, price}) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{`$${price}`}</span>
             </div>
+            <CustomButton inverted onClick={() => addItem(item)}>ADD TO CART</CustomButton>
         </div>
     )
 }
 
-export default CollectionItem
+const mapDispatchToProps = dispatch =>( {
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps) (CollectionItem);
